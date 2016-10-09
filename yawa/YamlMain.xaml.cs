ab9ce3a14063282ae0d724043df5ce0eac1f7945
Adam Cardenas <cevaris@gmail.com>
2016-10-09 10:00:08 -0600

    added weather hour detail new model

--- yawa/YamlMain.xaml.cs
+++ yawa/YamlMain.xaml.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using yawa.Models;
+
 
 namespace yawa
 {
 	public partial class YamlMain : ContentPage
 	{
+
+		ObservableCollection<WeatherHourDetail> weatherHourDetails = new ObservableCollection<WeatherHourDetail>();
+
 		public YamlMain()
 		{
+			weatherHourDetails.Add(new WeatherHourDetail { 
+				Hour = DateTime.UtcNow,
+				Temp = 999,
+				WindSpeed = 999,
+				WindDirection = "NW"
+			});
+
+
 			InitializeComponent();
 		}
 	}
